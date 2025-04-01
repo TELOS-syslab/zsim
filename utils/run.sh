1af769ee71c0f2ef3c408bc60fb9954c7d0d1c6f
chunk <chunk9@163.com>
2025-04-01 11:44:10 +0800

    stage

--- utils/run.sh
+++ utils/run.sh
@@ -8,37 +8,45 @@ fi
 
 
 
-cache_scheme=$1
-category=$2
+benchmark=""
+category=$1
+if [[ $category == "dramsim" || $category == "ddr" ]]; then
+    benchmark=$2
+    cache_scheme=$3
+else
+    cache_scheme=$2
+fi
+
 
 #echo 18446744073692774399 > /proc/sys/kernel/shmmax
 sudo sysctl -w kernel.shmmax=18446744073692774399
 sudo sysctl -w kernel.yama.ptrace_scope=0
 
 cd $ZSIMPATH
-
-# ./bin/zsim tests/debug.cfg output
-# ./bin/zsim tests/debug-dramsim2.cfg output
-# ./bin/zsim tests/debug-dramsim3.cfg output
-# ./bin/zsim tests/debug-ddr-dramsim3.cfg output
-# ./bin/zsim tests/simple_dramsim2.cfg output
-#  ./bin/zsim tests/simple_dramsim3.cfg output
-
-# ./bin/zsim tests/test-alloy.cfg output alloy-pr
-# ./bin/zsim tests/test-ndc-8192.cfg output ndc
-# ./bin/zsim tests/test-ndc-4096.cfg output ndc-pr
-# ./bin/zsim tests/test-ndc-2048.cfg output ndc
-# ./bin/zsim tests/test-nocache.cfg output nocache
-# ./bin/zsim tests/test-cacheonly.cfg output cacheonly
-# ./bin/zsim tests/test-debug.cfg output debug
-# ./bin/zsim tests/test-idealbalanced.cfg output idealbalanced-pr
-if [ $category == "benchmarks" ]; then
-    benchmark="default"
-    if [ $# -ge 3 ]; then
-        benchmark=$3
-    fi
-    ./bin/zsim tests/$category/$benchmark/test-$cache_scheme.cfg output $cache_scheme-$benchmark
-else
-    ./bin/zsim tests/$category/test-$cache_scheme.cfg output $cache_scheme
+if [ ! -d "output" ]; then
+    mkdir output
 fi
+if [ ! -d "tests/$category" ]; then
+    echo "No such test $category"
+    exit 1
+fi
+
+if [ $benchmark != "" ]; then
+    if [ ! -d "tests/$category/$benchmark" ]; then
+        echo "No such test $category/$benchmark"
+        exit 1
+    fi
+    if [ ! -f "tests/$category/$benchmark/test-$cache_scheme.cfg" ]; then
+        echo "No such test $category/$benchmark/test-$cache_scheme.cfg"
+        exit 1
+    fi
+    ./bin/zsim tests/$category/$benchmark/test-$cache_scheme.cfg output $cache_scheme-$benchmark-$category
+else
+    if [ ! -f "tests/$category/test-$cache_scheme.cfg" ]; then
+        echo "No such test $category/test-$cache_scheme.cfg"
+        exit 1
+    fi
+    ./bin/zsim tests/$category/test-$cache_scheme.cfg output $cache_scheme-$category
+fi
+
 cd -
